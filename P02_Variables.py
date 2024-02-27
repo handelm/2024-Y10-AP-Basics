db9c5f1c9aa48f29206e6dbafe4ac534203bf00c
handelm <161265311+handelm@users.noreply.github.com>
2024-02-28 09:08:23 +1300

    Add files via upload

---
+++ P02_Variables.py
@@ -0,0 +1,17 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+
+# Variables can be Boolean (True / False)
+Love_programing = True
+
+# This Variable has a Problem!
+# my name = "Miss G"
+
+print(greeting)
+print(fav_num)
+print(Love_programing)
